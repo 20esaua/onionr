c0ce7100307c7abb4374793468be85a9207c5c92
Kevin Froman <kevinfroman12@gmail.com>
2017-12-29 11:49:06 -0600

    fixed TimedHMAC init, but defunct

--- timedHmac.py
+++ timedHmac.py
@@ -12,6 +12,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
+
+# Defunct class (and unfinished) since we will just store hashes of HMAC session payloads
+
 import hmac, base64, time, math
 class TimedHMAC:
     def __init__(self, base64Key, data, hashAlgo):
@@ -27,17 +30,25 @@ class TimedHMAC:
         self.expire = math.floor(time.time())
         self.hashAlgo = hashAlgo
         self.b64Key = base64Key
-        generatedHMAC = hmac.HMAC(base64.b64decode(base64Key).decode(), digestmod=self.hashAlgo)
-        generatedHMAC.update(data + expire)
+        generatedHMAC = hmac.HMAC(base64.b64decode(base64Key), digestmod=self.hashAlgo)
+        generatedHMAC.update(data.encode() + str(self.expire).encode())
         self.HMACResult = generatedHMAC.hexdigest()
         return
 
     def check(self, data):
         # Check a hash (and verify time is sane)
-        testHash = hmac.HMAC(base64.b64decode(base64Key).decode(), digestmod=self.hashAlgo)
-        testHash.update(data + math.floor(time.time()))
+        testHash = hmac.HMAC(base64.b64decode(self.b64Key), digestmod=self.hashAlgo)
+        testHash.update(data.encode() + str(math.floor(time.time())).encode())
         testHash = testHash.hexdigest()
         if hmac.compare_digest(testHash, self.HMACResult):
-            return true
+            return True
         else:
-            return false
+            for i in range(20):
+                testHash = hmac.HMAC(base64.b64decode(self.b64Key), digestmod=self.hashAlgo)
+                testHash.update(data.encode() + str(math.floor(time.time()) - i).encode())
+                testHash = testHash.hexdigest()
+                if hmac.compare_digest(testHash, self.HMACResult):
+                    break
+            else:
+                return False
+            return True
